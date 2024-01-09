e0ad6f937ba9b2c2bb01770ebf45110e734a6eaa
andyr <andyr@LAPTOP-JKVTPKV5>
2024-01-09 18:32:48 -0500

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Project4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab1 = new System.Windows.Forms.TabPage();
+            this.gridData = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tab2 = new System.Windows.Forms.TabPage();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.lstGrade = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tab1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).BeginInit();
+            this.tab2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tab1);
+            this.tabControl1.Controls.Add(this.tab2);
+            this.tabControl1.Location = new System.Drawing.Point(43, 26);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 346);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            this.tabControl1.TabIndexChanged += new System.EventHandler(this.tabControl1_TabIndexChanged);
+            // 
+            // tab1
+            // 
+            this.tab1.Controls.Add(this.gridData);
+            this.tab1.Controls.Add(this.btnAdd);
+            this.tab1.Controls.Add(this.btnDisplay);
+            this.tab1.Controls.Add(this.txtScore);
+            this.tab1.Controls.Add(this.txtID);
+            this.tab1.Controls.Add(this.txtName);
+            this.tab1.Controls.Add(this.lblScore);
+            this.tab1.Controls.Add(this.lblID);
+            this.tab1.Controls.Add(this.lblName);
+            this.tab1.Location = new System.Drawing.Point(4, 22);
+            this.tab1.Name = "tab1";
+            this.tab1.Padding = new System.Windows.Forms.Padding(3);
+            this.tab1.Size = new System.Drawing.Size(767, 320);
+            this.tab1.TabIndex = 0;
+            this.tab1.Text = "Student Info";
+            this.tab1.UseVisualStyleBackColor = true;
+            // 
+            // gridData
+            // 
+            this.gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridData.Location = new System.Drawing.Point(256, 73);
+            this.gridData.Name = "gridData";
+            this.gridData.Size = new System.Drawing.Size(487, 227);
+            this.gridData.TabIndex = 8;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(441, 29);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(87, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add Data";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(278, 29);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(87, 23);
+            this.btnDisplay.TabIndex = 6;
+            this.btnDisplay.Text = "Display Data";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(103, 116);
+            this.txtScore.Multiline = true;
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 5;
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(103, 73);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(103, 29);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(45, 116);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(36, 15);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(19, 73);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(62, 15);
+            this.lblID.TabIndex = 1;
+            this.lblID.Text = "Student ID";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(42, 29);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 15);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // tab2
+            // 
+            this.tab2.Controls.Add(this.btnExport);
+            this.tab2.Controls.Add(this.lstGrade);
+            this.tab2.Location = new System.Drawing.Point(4, 22);
+            this.tab2.Name = "tab2";
+            this.tab2.Padding = new System.Windows.Forms.Padding(3);
+            this.tab2.Size = new System.Drawing.Size(767, 320);
+            this.tab2.TabIndex = 1;
+            this.tab2.Text = "Student Grade";
+            this.tab2.UseVisualStyleBackColor = true;
+            this.tab2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tab2_MouseClick);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(487, 34);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(87, 23);
+            this.btnExport.TabIndex = 7;
+            this.btnExport.Text = "Export Data";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lstGrade
+            // 
+            this.lstGrade.FormattingEnabled = true;
+            this.lstGrade.Location = new System.Drawing.Point(32, 34);
+            this.lstGrade.Name = "lstGrade";
+            this.lstGrade.Size = new System.Drawing.Size(393, 238);
+            this.lstGrade.TabIndex = 0;
+            this.lstGrade.SelectedIndexChanged += new System.EventHandler(this.lstGrade_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "Student App";
+            this.tabControl1.ResumeLayout(false);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridData)).EndInit();
+            this.tab2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tab1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TabPage tab2;
+        private System.Windows.Forms.DataGridView gridData;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ListBox lstGrade;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient; //References the use of MySql
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project4
+{
+    public partial class MainForm : Form
+    {
+        string connstring = "Server=localhost;Database=project4db;User=root;Password=iHateminions9!";
+        MySqlConnection connection; //Global connection object
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenDbConnection()
+        {
+            try
+            {
+                //creates a new database connection
+                connection = new MySqlConnection(connstring);
+                connection.Open(); //opens the db connection
+            }
+            catch (Exception ex) //gives us the exact error
+            {
+                MessageBox.Show("There was an error in establishing the connection: " + ex.Message); //concat. the actual message error
+            }
+        }
+
+        public void CloseDbConnection()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+
+        {
+            //Checks if any of the required fields are empty
+            if (string.IsNullOrWhiteSpace(txtID.Text) || string.IsNullOrWhiteSpace(txtName.Text) || string.IsNullOrWhiteSpace(txtScore.Text))
+            {
+                MessageBox.Show("Please fill out all fields.");
+                return;
+            }
+
+            OpenDbConnection();
+
+            //gets values from the texboxes
+            string studentName = txtName.Text;
+            int studentId, score;
+
+            //parses the ID and scores as integers
+            int.TryParse(txtID.Text, out studentId);
+            int.TryParse(txtScore.Text, out score);
+
+            //SQL statement for inserting data into studentdata table
+            string sqlStatement = "insert into studentdata(studentid, studentname, score) values ('" + studentId + "',  '" + studentName + "',  '" + score + "')";
+            //executes the command
+            MySqlCommand sqlcommand = new MySqlCommand(sqlStatement, connection);
+            sqlcommand.ExecuteNonQuery();
+            MessageBox.Show("Added data to the Student Data table");
+
+            CloseDbConnection();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            OpenDbConnection();
+
+            //this creates the sql statemnt to be executed as a string
+            string sqlStatement = "select * from studentdata";
+
+            //Create a MySqlCommand option
+            //Paremters are the Sql Statement and the db connection object
+            MySqlCommand sqlCommand = new MySqlCommand(sqlStatement, connection);
+            //Creates a MySqlDataReader object that reads data from the table
+            MySqlDataReader reader = sqlCommand.ExecuteReader();
+            //This creates a DataTable that stores row and column in memory
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            gridData.DataSource = dt;
+
+
+            CloseDbConnection();
+        }
+
+        private void tab2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void lstGrade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+
+        {
+            try
+            {
+                // Open a StreamWriter to write to the file
+                using (StreamWriter writer = new StreamWriter("studentgrades.txt"))
+                {
+                    OpenDbConnection();
+
+                  //SQL statement to select allthe data from studentdata table
+                    string sqlCommand = "select * from studentdata";
+                    MySqlCommand command = new MySqlCommand(sqlCommand, connection);
+                    MySqlDataReader reader = command.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        // Converts objects to integers
+                        int id = Convert.ToInt32(row["studentid"]);
+                        string name = row["studentname"].ToString();
+                        int score = Convert.ToInt32(row["score"]);
+                        string grade;
+
+                        //Determine the grade based on the score
+                        if (score < 60)
+                        {
+                            grade = "F";
+                        }
+                        else if (score >= 60 && score <= 69)
+                        {
+                            grade = "D";
+                        }
+                        else if (score >= 70 && score <= 79)
+                        {
+                            grade = "C";
+                        }
+                        else if (score >= 80 && score <= 89)
+                        {
+                            grade = "B";
+                        }
+                        else // score is between 90 and 100
+                        {
+                            grade = "A";
+                        }
+
+                        // Write student data to the text file
+                        writer.WriteLine($"{id}\t{name}\t{grade}");
+                    }
+
+                    MessageBox.Show("Data exported to studentgrades.txt");
+                    CloseDbConnection();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while exporting data: " + ex.Message);
+            }
+        }
+
+        private void tabControl1_TabIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            OpenDbConnection();
+
+            //SQL statement to select all data from the studentdata table
+            string sqlCommand = "select * from studentdata";
+            MySqlCommand command = new MySqlCommand(sqlCommand, connection);
+            MySqlDataReader reader = command.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+            //puts student data into the lstGrade listbox
+            foreach (DataRow row in dt.Rows)
+            {
+                //Converts objects to integers
+                int id = Convert.ToInt32(row["studentid"]);
+                string name = row["studentname"].ToString();
+                int score = Convert.ToInt32(row["score"]);
+                string grade;
+
+                //Determine the grade based on the score
+                if (score < 60)
+                {
+                    grade = "F";
+                }
+                else if (score >= 60 && score <= 69)
+                {
+                    grade = "D";
+                }
+                else if (score >= 70 && score <= 79)
+                {
+                    grade = "C";
+                }
+                else if (score >= 80 && score <= 89)
+                {
+                    grade = "B";
+                }
+                else // score is between 90 and 100
+                {
+                    grade = "A";
+                }
+
+               //Add student data to the lstGrade listbox
+                lstGrade.Items.Add(string.Format("{0,20}{1,20}{2,20}", id, name, grade));
+
+
+            }
+
+            CloseDbConnection();
+
+        }
+    }
+}
